67a06ccc76f7d7e80572cbc527ab76ec088619d6
iamarjun <arjunmanoj1995@gmail.com>
2020-10-20 23:33:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/screens/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFF0C54BE),
+        accentColor: Color(0xFF303F60),
+        scaffoldBackgroundColor: Color(0xFFF5F9FD),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/service/api_service.dart
@@ -0,0 +1,7 @@
+class ApiService {
+  static final String _baseUrl = 'https://newsapi.org/v2/';
+  static final String _apiKey = '84a072f692a848689b11ff35496cddcc';
+
+  
+
+}
